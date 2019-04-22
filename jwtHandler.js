9b69256445cbf98601abff0c2c76da73b6de75fc
Henry Akmnn <redii@users.noreply.github.com>
2019-04-22 14:47:22 +0200

    bcrypt fix

--- jwtHandler.js
+++ jwtHandler.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const saltRounds = 10
 const mongoose = require('./mongooseHandler')
 const UserSchema = require('./models/User')
